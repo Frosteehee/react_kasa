ed18dac384fb27098f8c43725d58796ea2f903d5
Marine Allavoine <allavoine.marine@gmail.com>
2024-06-13 09:57:39 +0200

    Correction largeur collapse sur ordi de petite taille

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({ 
 
+  base: process.env.NODE_ENV === 'production' ? "/react_kasa" : "/", // Spécifie le chemin de base pour le déploiement sur GitHub Pages
   plugins: [react()],
-  base: "/project_react/"
 })
 
